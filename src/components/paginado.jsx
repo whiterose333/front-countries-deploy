5b94336aff872d3ace5f89077ec39b89d09e0e95
Sebastian Garcia <93052003+whiterose333@users.noreply.github.com>
2023-01-25 19:57:32 -0500

    Update paginado.jsx

--- src/components/paginado.jsx
+++ src/components/paginado.jsx
@@ -3,22 +3,23 @@ import styles from "./home.module.css"
 
 
 
-export default function Paginado ({countriesPerPage, allCountries, paginado}){
+export default function Paginado({ countriesPerPage, allCountries, paginado }) {
     const pageNumbers = []
 
-    for (let i= 0; i < Math.ceil(allCountries/countriesPerPage); i++){
-        pageNumbers.push(i+1)
+    for (let i = 0; i < Math.ceil(allCountries / countriesPerPage); i++) {
+        pageNumbers.push(i + 1)
     }
 
-    return(
+    return (
         <nav>
             <ul className={styles.paginado}>
                 {pageNumbers && pageNumbers.map(number => {
-                return(
-                    
-                        <li key= {number}>
-                        <button className={styles.btnNumber} onClick= {() => paginado(number)}>{number}</button>
-                        </li>   
+                    return (
+                        <div className={styles.pagina}>
+                            <li key={number}>
+                                <button className={styles.btnNumber} onClick={() => paginado(number)}>{number}</button>
+                            </li>
+                        </div>
                     )
                 })}
             </ul>
